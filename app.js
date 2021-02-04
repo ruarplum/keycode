03f0b832996b3de06351050bbd3e2acbac88cf11
Ruaraidh <rca.plummer@gmail.com>
2021-02-04 12:35:24 +0000

    #4 app.js variables (Const) created

---
+++ app.js
@@ -0,0 +1,8 @@
+const body = document.querySelector("body");
+const firstPage = document.querySelector(".first-page");
+const introText = document.querySelector(".intro-text");
+const eventTitles = document.querySelector(".event-titles");
+const eventKeyResult = document.querySelector(".key-result");
+const eventCodeResult = document.querySelector(".code-result");
+const eventWhichResult = document.querySelector(".which-result");
+
